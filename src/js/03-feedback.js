abdfbff78b9c806b1773843dd617a9972b30b680
Oleksii Verbytskyi <alverba88@gmail.com>
2022-02-02 09:34:51 +0200

    fixed bugs according mentor notes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,25 +6,28 @@ const formMessageRef = document.querySelector('.feedback-form'),
 
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
-const formDataStorage = {};
-
 const fomrSubmitHandler =(e)=>{
     e.preventDefault();
+    const enteredData =new FormData (e.currentTarget);
+    enteredData.forEach((value, name)=>{
+        console.log(`${name}: ${value}`);
+    })
     e.currentTarget.reset();
     localStorage.removeItem(LOCALSTORAGE_KEY);
-    console.log(`E-mail: ${formDataStorage.email}`)
-    console.log(`Message: ${formDataStorage.message}`)
     
 }
-const formInputHandler=(e)=>{
-    formDataStorage[e.target.name] = e.target.value;
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formDataStorage));   
+const formInputHandler=()=>{
+    const formValuesStorage = {
+        email: inputEl.value,
+        message: commentEl.value,
+      };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formValuesStorage));   
 }
 
 function showPreviosDataForm() {
     const messageObj = localStorage.getItem(LOCALSTORAGE_KEY)
     const message = JSON.parse(messageObj);
-    if (message) {
+    if(message){
         inputEl.value = message.email || '';
         commentEl.value = message.message || '';
     }
